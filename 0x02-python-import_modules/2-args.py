1ed9622f95c1f6f0f31bfd200df78675ded0c564
Ernest Sinyangwe <ernest.s.sinyangwe@gmail.com>
2022-12-02 18:15:59 +0200

    fix Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-	import sys
-	arg_count = len(sys.argv)
-	if arg_count == 1:
-		print("0 arguments.")
-	if arg_count > 1:
-		count = len(sys.argv) - 1
-		print("{} argument:".format(count))
-		index = 1
-		while index <= count:
-			argument = sys.argv[index]
-			print("{}: {}".format(index, argument))
-			index += 1
+    import sys
+    arg_count = len(sys.argv)
+    if arg_count == 1:
+        print("0 arguments.")
+    if arg_count > 1:
+        count = len(sys.argv) - 1
+        print("{} argument:".format(count))
+        index = 1
+        while index <= count:
+            argument = sys.argv[index]
+            print("{}: {}".format(index, argument))
+            index += 1
